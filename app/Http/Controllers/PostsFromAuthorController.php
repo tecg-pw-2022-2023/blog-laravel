20b0e9c0167d67f04cbf82ebd2788f2eb4ab0cd7
Dominique Vilain <dominique.vilain@hepl.be>
2023-02-11 18:07:21 +0100

    add .idea to .gitignore

--- app/Http/Controllers/PostsFromAuthorController.php
+++ app/Http/Controllers/PostsFromAuthorController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\Foundation\Application;
